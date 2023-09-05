2fed3168a2a2c24c89efbf5faede52ba0d29a4bc
Urooj-Fatima-Memon <138853875+Urooj-Fatima-Memon@users.noreply.github.com>
2023-09-05 14:07:11 +0500

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+
+import Pages from './component/pages/home/pages'
+import styles from './page.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Script from 'next/script'
+
+export default function Home() {
+  return (
+
+       
+<>
+<div>
+<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/> 
+  
+      <Pages />
+    </div>
+
+</>
+  )
+}
